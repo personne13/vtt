631f5a7925dfbcab035a121feaf045dbf3203eb7
Thomas Damiani <thomasdamiani13@gmail.com>
2018-10-17 11:15:24 +0200

    base repository

--- td2/eq-histogram.cpp
+++ td2/eq-histogram.cpp
@@ -8,32 +8,24 @@ using namespace cv;
 using namespace std;
 
 void
-process(const char* imsname,
-	int i0,
-	int j0,
-	int w,
-	int h)
+process(const char* imsname)
 {
   (void) imsname;
-  (void) i0;
-  (void) j0;
-  (void) w;
-  (void) h;
 }
 
 void
 usage (const char *s)
 {
-  std::cerr<<"Usage: "<<s<<" imsname i0 j0 w h\n"<<std::endl;
+  std::cerr<<"Usage: "<<s<<" imsname\n"<<std::endl;
   exit(EXIT_FAILURE);
 }
 
-#define param 5
+#define param 1
 int
 main( int argc, char* argv[] )
 {
   if(argc != (param+1))
     usage(argv[0]);
-  process(argv[1], atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+  process(argv[1]);
   return EXIT_SUCCESS;
 }
