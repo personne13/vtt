e77655a28d5cfff06cab5b4998b093d1436e6ed9
TRIAT Franck <ftriat@kramnik.pedago.ipb.fr>
2018-10-17 12:04:09 +0200

    oui

--- td2/color-convert.cpp
+++ td2/color-convert.cpp
@@ -11,6 +11,29 @@ void
 process(const char* imsname)
 {
   (void) imsname;
+  Mat rgb[3];
+  Mat YCbCr[3];
+	Mat image_src = imread(imsname, CV_LOAD_IMAGE_COLOR);
+
+
+	if(!image_src.data){
+      cout <<  "Could not open or find the image" << std::endl ;
+      return;
+		}
+    // Mat ycrcb = cv2.cvtColor(img, cv2.COLOR_BGR2YCR_CB)
+
+	split(image_src, rgb);
+	imshow("R", rgb[0]);
+	imshow("G", rgb[1]);
+	imshow("B", rgb[2]);
+  Mat ycrcb;
+  cvtColor(image_src,ycrcb,COLOR_BGR2YCrCb);
+  split(ycrcb, YCbCr);
+	imshow("Y", YCbCr[0]);
+	imshow("Cr", YCbCr[1]);
+	imshow("Cb", YCbCr[2]);
+
+  waitKey(0);
 }
 
 void
--- td2/hsv-modification.cpp
+++ td2/hsv-modification.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <cstdlib>
 #include <math.h>
-
 #include <opencv2/opencv.hpp>
 
 using namespace cv;
@@ -14,12 +13,29 @@ process(int h,
 	char* ims,
 	char* imd)
 {
-  (void)  h,
-  (void) s,
-  (void) v,
-  (void) ims,
-  (void) imd;
+	Mat hsv[3];
+	Mat image_src = imread(ims, CV_LOAD_IMAGE_COLOR);
+	Mat img_dst;
+	Mat img_hsv;
 
+	if(!image_src.data){
+      cout <<  "Could not open or find the image" << std::endl ;
+      return;
+		}
+
+	cvtColor(image_src,img_hsv,CV_RGB2HSV);
+
+	split(img_hsv, hsv);
+	imshow("h", hsv[0]);
+	imshow("s", hsv[1]);
+	imshow("v", hsv[2]);
+
+	Mat tmp(img_hsv.rows, img_hsv.cols, CV_8UC3, Scalar(h, s, v));
+	img_hsv = img_hsv + tmp;
+	cvtColor(img_hsv, img_dst, CV_HSV2RGB);
+	imwrite(imd, img_dst);
+
+	waitKey(0);
 }
 
 void
