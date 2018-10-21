647ba22347d99f6ae2e99549942d2092d72f6c1f
ftriat <franck.triat@gmail.com>
2018-10-21 20:59:11 +0200

    otsu pas parfait

--- td2/otsu.cpp
+++ td2/otsu.cpp
@@ -7,10 +7,161 @@
 using namespace cv;
 using namespace std;
 
+void computeHistogram(std::vector<int> &H, Mat &src) {
+    for (int j = 0; j < (int)H.size(); j++) {
+      H[j] = 0;
+    }
+
+    MatIterator_<uchar> end = src.end<uchar>();
+    for (MatIterator_<uchar> i = src.begin<uchar>(); i != end; i++) {
+      uchar k = i[0];
+      H[k]++;
+    }
+}
+
+void computeCumulatedNormalizedHistogram(std::vector<float> &Hcn,std::vector<int> &H, float MN) {
+  int size = H.size();
+  Hcn[0] = H[0]/MN;
+  for (int i = 1; i < size; i++) {
+    Hcn[i] = Hcn[i-1] + H[i]/MN;
+  }
+
+}
+
+
+float compute_p1(std::vector<int> &H, int t, float MN) {
+  float p1 = 0;
+  for (int i = 0; i < t; i++) {
+    p1 += H[i];
+  }
+  p1 = p1/MN;
+  return p1;
+}
+
+float compute_p2(std::vector<int> &H, int t, float MN) {
+  float p2 = 0;
+  for (int i = t + 1; i < 256; i++) {
+    p2 += H[i];
+  }
+  p2 = p2/MN;
+  return p2;
+}
+
+float compute_u1(std::vector<int> &H, int t) {
+  float u1 = 0;
+  float g = 0;
+  float d = 0;
+  for (int i = 0; i < t; i++) {
+    g += H[i];
+    d += i * H[i];
+  }
+  if (g == 0) {
+    return 0;
+  }
+  else {
+    u1 = 1/g * d;
+    return u1;
+  }
+}
+
+float compute_u2(std::vector<int> &H, int t) {
+  float u2 = 0;
+  float g = 0;
+  float d = 0;
+  for (int i = t + 1; i < 256; i++) {
+    g += H[i];
+    d += i * H[i];
+  }
+  if (g == 0) {
+    return 0;
+  }
+  else {
+  u2 = 1/g * d;
+  return u2;
+}
+}
+
+float compute_variance (float p1, float p2, float u1, float u2) {
+  return p1 * p2 * pow((u1 - u2), 2);
+}
+
+void otsu (Mat &src_gray) {
+  std::vector<int> H(256);
+  std::vector<float> Hcn(256);
+  float MN = src_gray.rows * src_gray.cols;
+
+  computeHistogram(H, src_gray);
+  computeCumulatedNormalizedHistogram(Hcn, H, MN);
+
+
+  float p_1[256], p_2[256], u_1[256], u_2[256];
+
+  for (int i = 0; i < 255; i++)
+  {
+    p_1[i] = compute_p1(H, i, MN);
+    p_2[i] = compute_p2(H, i, MN);
+    u_1[i] = compute_u1(H, i);
+    u_2[i] = compute_u2(H, i);
+  }
+
+    float variance_max = 0;
+  	float best = 0;
+  	for (int t = 0; t < 254; t++)
+  	{
+      int vk = compute_variance(p_1[t], p_2[t], u_1[t], u_2[t]);
+
+  		if (vk > variance_max)
+  		{
+  			variance_max = vk;
+  			best = t;
+  		}
+  	}
+    std::cout << "manual otsu threshold t = " << best << std::endl;
+  	for (int i = 0; i < src_gray.rows; i++)
+  	{
+  		for (int j = 0; j < src_gray.cols; j++)
+  		{
+  			if (src_gray.at<uchar>(i, j) < best)
+  			{
+  				src_gray.at<uchar>(i, j) = 0;
+  			}
+  			else
+  			{
+  				src_gray.at<uchar>(i, j) = 255;
+  			}
+  		}
+  }
+  std :: cout << "init done" << std::endl;
+}
+
 void
 process(const char* imsname)
 {
-  (void) imsname;
+
+  Mat src_gray = imread(imsname, CV_LOAD_IMAGE_GRAYSCALE);
+  Mat diff;
+  Mat src_gray_opencv = imread(imsname, CV_LOAD_IMAGE_GRAYSCALE);
+	if(!src_gray.data){
+      cout <<  "Could not open or find the image" << std::endl ;
+      return;
+		}
+
+    otsu(src_gray);
+
+
+  imshow("otsu-th", src_gray);
+  imwrite("otsu-th.png", src_gray);
+
+  // open cv
+  Mat otsu_ocv;
+  int thresh = threshold(src_gray_opencv, otsu_ocv, 0, 255, THRESH_BINARY+THRESH_OTSU);
+  std::cout << "ocv otsu threshold t = " << thresh << std::endl;
+  imshow("otsu-th-ocv", otsu_ocv);
+  imwrite("otsu-th-ocv.png", otsu_ocv);
+
+  diff = src_gray - otsu_ocv;
+  imshow("diff", diff);
+  waitKey(0);
 }
 
 void
